30133eaff67ca401d3ef2dd1d21ddf50716519ff
Fredy Augusto Castellón Bedoya <castellonbedoya@gmail.com>
2021-09-01 22:54:18 -0600

    SQL Database Table Creation

---
+++ timco-retail-manager/TRMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL IDENTITY , 
+    [SaleId] INT NOT NULL, 
+    [ProductId] INT NOT NULL,
+    [Quantity] INT NOT NULL DEFAULT 1,
+    [PurchasePrice] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [PK_SaleDetail] PRIMARY KEY ([Id]) 
+)
